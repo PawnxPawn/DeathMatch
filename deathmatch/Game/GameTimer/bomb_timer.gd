3a17dee4d90317553768857b37cfc2c4bcce6e78
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2025-04-29 13:36:24 -0600

    [Minor] Added spark to fuse
    NOTE: Spark does not follow the fuse path properly. need to come up with new plan to make it look better to follow.

--- deathmatch/Game/GameTimer/bomb_timer.gd
+++ deathmatch/Game/GameTimer/bomb_timer.gd
@@ -7,10 +7,9 @@ signal timed_out
 @export var timer_wait_time:float = 30.0
 
 @onready var game_timer: Timer = $GameTimer
-@onready var texture_progress_bar: TextureProgressBar = $BombAnimated/TextureProgressBar
+@onready var texture_progress_bar: TextureProgressBar = %FuseTimer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-
+@onready var fuse_path_follow: PathFollow2D = %SparkFusePath
 # Set wait time then start the timer
 func _ready() -> void:
 	game_timer.wait_time = timer_wait_time
@@ -21,7 +20,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	var timer_percentage = game_timer.time_left / timer_wait_time
 	texture_progress_bar.value = timer_percentage
-
+	fuse_path_follow.progress_ratio = timer_percentage - .01
 
 func _on_game_timer_timeout() -> void:
 	animation_player.play(&"explode")
