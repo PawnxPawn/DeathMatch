748cedc40ed148fe2aa66e9768a21588d7b01ea1
Vhal Ragnarok <bilbobaggins30@protonmail.com>
2025-05-04 10:02:53 -0500

    Fixed critical error. Change Cursor is deprecated.

--- deathmatch/Menus/settings_menu.gd
+++ deathmatch/Menus/settings_menu.gd
@@ -5,9 +5,8 @@ signal return_control
 func _on_return_button_pressed() -> void:
 	return_control.emit()
 
-func _on_return_button_mouse_entered() -> void:
-	GameManager.change_cursor(true)
+#func _on_return_button_mouse_entered() -> void:
+#	GameManager.change_cursor(true)
 
-func _on_return_button_mouse_exited() -> void:
-	GameManager.change_cursor(false)
-
+#func _on_return_button_mouse_exited() -> void:
+#	GameManager.change_cursor(false)
