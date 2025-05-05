ee246dff08cae5bac72c6537b9caa5bc0307b487
PawnxPawn <pawnxpawnofficial+github@gmail.com>
2025-05-04 18:45:35 -0600

    [Major] Added selector, fixed bugs
    + Added Selector to all menus and main game.
    + Fixed a bug where the game wouldn't unpause when reentering after going into the main menu
    + Fixed a bug where game stats didn't reset when going to main menu and reentering the game.

--- deathmatch/Menus/settings_menu.gd
+++ deathmatch/Menus/settings_menu.gd
@@ -1,9 +1,48 @@
 extends Control
 
+@export_category("Selector")
+@export var selector: PanelContainer
+@export var selector_offset: Vector2
+
 signal return_control
 
 func _on_return_button_pressed() -> void:
+	selector.hide()
 	return_control.emit()
+
+
+func _on_mouse_entered() -> void:
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	var node: Node
+
+	for child in %SettingsMenuContainer.get_children():
+		if (child is Button or child is HSlider) and child.get_global_rect().has_point(mouse_pos):
+			node = child
+			break
+
+		for sub_child in child.get_children():
+			if sub_child is Button or sub_child is HSlider:
+				if sub_child.get_global_rect().has_point(mouse_pos):
+					node = sub_child
+					break
+	
+	if node == null: return
+
+	if node is HSlider:
+		node = node.get_parent().get_child(0)
+
+	var node_center_point: Vector2 = node.get_global_rect().position
+	node_center_point.x -= node.get_global_rect().size.x
+
+
+	selector.set_selector_position(node_center_point, selector_offset)
+	selector.show()
+
+
+func _on_mouse_exited() -> void:
+	#selector.hide()
+	pass
+	
 
 #func _on_return_button_mouse_entered() -> void:
 #	GameManager.change_cursor(true)
